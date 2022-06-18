7d680a3fe33c563a5582ef9fdf7cac3165a1e9f3
EricSu <854612607@qq.com>
2022-06-18 23:32:33 +0800

    hot-fix

--- src/main/java/com/tencent/test/GitMain.java
+++ src/main/java/com/tencent/test/GitMain.java
@@ -6,6 +6,8 @@ public class GitMain {
 
         System.out.println("hello git 2");
         System.out.println("hello git 3");
+        System.out.println("hot fix demo");
+        System.out.println("hot fix end");
 
     }
 }
