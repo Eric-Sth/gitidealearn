1cf7ec323a690712b99b2426cc4231cb21317520
EricSu <854612607@qq.com>
2022-06-18 19:19:14 +0800

    test02

--- src/main/java/com/tencent/test/GitMain.java
+++ src/main/java/com/tencent/test/GitMain.java
@@ -4,5 +4,8 @@ public class GitMain {
     public static void main(String[] args) {
         System.out.println("git test");
 
+        System.out.println("hello git 2");
+        System.out.println("hello git 3");
+
     }
 }
